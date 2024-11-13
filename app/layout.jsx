96dade3a7014183f74f2aaea2b7ec5222eda26ef
AndrejErjavec <erandrej@gmail.com>
2024-11-13 23:03:30 +0100

    basic landing page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,25 @@
-import { Inter } from 'next/font/google';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import AuthWrapper from '@/components/AuthWrapper';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '@/assets/styles/globals.css';
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import AuthWrapper from "@/components/AuthWrapper";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Bookit App | Book a Room',
-  description: 'Book a meeting or conference room for your team',
+  title: "Obalni alpinistični klub",
+  description: "Spletna stran Obalnega aplinističnega kluba",
 };
 
 export default function RootLayout({ children }) {
   return (
     <AuthWrapper>
-      <html lang='en'>
+      <html lang="en">
         <body className={inter.className}>
           <Header />
-          <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
-            {children}
-          </main>
+          <main className="">{children}</main>
           <Footer />
           <ToastContainer />
         </body>
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,17 @@
-'use client';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import Link from 'next/link';
-import logo from '@/assets/images/logo.svg';
-import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from 'react-icons/fa';
-import { toast } from 'react-toastify';
-import destroySession from '@/app/actions/destroySession';
-import { useAuth } from '@/context/authContext';
+"use client";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from "react-icons/fa";
+import { toast } from "react-toastify";
+import destroySession from "@/app/actions/destroySession";
+import { useAuth } from "@/context/authContext";
 
 const Header = () => {
   const router = useRouter();
 
-  const { isAuthenticated, setIsAuthenticated, currentUser, setCurrentUser } = useAuth();
+  const { isAuthenticated, setIsAuthenticated, currentUser, setCurrentUser } =
+    useAuth();
 
   const handleLogout = async () => {
     const { success, error } = await destroySession();
@@ -19,30 +19,32 @@ const Header = () => {
     if (success) {
       setIsAuthenticated(false);
       setCurrentUser({});
-      router.push('/login');
+      router.push("/login");
     } else {
       toast.error(error);
     }
   };
 
   return (
-    <header className='bg-gray-100'>
-      <nav className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-        <div className='flex h-16 items-center justify-between'>
-          <div className='flex items-center'>
-            <Link href='/'>
+    <header className="bg-gray-100">
+      <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <Link href="/">
               <Image
-                className='h-12 w-12'
-                src={logo}
-                alt='Bookit'
+                className="h-12 w-12"
+                src="/oak-logo.png"
+                width={191}
+                height={183}
+                alt="Bookit"
                 priority={true}
               />
             </Link>
-            <div className='hidden md:block'>
-              <div className='ml-10 flex items-baseline space-x-4'>
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
                 <Link
-                  href='/'
-                  className='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'
+                  href="/"
+                  className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                 >
                   Home
                 </Link>
@@ -50,14 +52,14 @@ const Header = () => {
                 {isAuthenticated && (
                   <>
                     <Link
-                      href='/'
-                      className='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'
+                      href="/"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                     >
                       Auth Only 1
                     </Link>
                     <Link
-                      href='/'
-                      className='rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white'
+                      href="/"
+                      className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                     >
                       Auth Only 1
                     </Link>
@@ -67,36 +69,36 @@ const Header = () => {
             </div>
           </div>
           {/* <!-- Right Side Menu --> */}
-          <div className='ml-auto'>
-            <div className='ml-4 flex items-center md:ml-6'>
+          <div className="ml-auto">
+            <div className="ml-4 flex items-center md:ml-6">
               {/* <!-- Logged Out Only --> */}
               {!isAuthenticated && (
                 <>
                   <Link
-                    href='/login'
-                    className='mr-3 text-gray-800 hover:text-gray-600'
+                    href="/login"
+                    className="mr-3 text-gray-800 hover:text-gray-600"
                   >
-                    <FaSignInAlt className='inline mr-1' /> Login
+                    <FaSignInAlt className="inline mr-1" /> Login
                   </Link>
                   <Link
-                    href='/register'
-                    className='mr-3 text-gray-800 hover:text-gray-600'
+                    href="/register"
+                    className="mr-3 text-gray-800 hover:text-gray-600"
                   >
-                    <FaUser className='inline mr-1' /> Register
+                    <FaUser className="inline mr-1" /> Register
                   </Link>
                 </>
               )}
 
               {isAuthenticated && (
                 <>
-                  <Link href='/'>
-                    <FaBuilding className='inline mr-1' /> My Profile
+                  <Link href="/">
+                    <FaBuilding className="inline mr-1" /> My Profile
                   </Link>
                   <button
                     onClick={handleLogout}
-                    className='mx-3 text-gray-800 hover:text-gray-600'
+                    className="mx-3 text-gray-800 hover:text-gray-600"
                   >
-                    <FaSignOutAlt className='inline mr-1' /> Sign Out
+                    <FaSignOutAlt className="inline mr-1" /> Sign Out
                   </button>
                 </>
               )}
@@ -106,11 +108,11 @@ const Header = () => {
       </nav>
 
       {/* <!-- Mobile menu --> */}
-      <div className='md:hidden'>
-        <div className='space-y-1 px-2 pb-3 pt-2 sm:px-3'>
+      <div className="md:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
           <Link
-            href='/'
-            className='block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white'
+            href="/"
+            className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
           >
             Rooms
           </Link>
@@ -118,14 +120,14 @@ const Header = () => {
           {isAuthenticated && (
             <>
               <Link
-                href='/bookings'
-                className='block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white'
+                href="/bookings"
+                className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
               >
                 Bookings
               </Link>
               <Link
-                href='/rooms/add'
-                className='block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white'
+                href="/rooms/add"
+                className="block rounded-md px-3 py-2 text-base font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
               >
                 Add Room
               </Link>
